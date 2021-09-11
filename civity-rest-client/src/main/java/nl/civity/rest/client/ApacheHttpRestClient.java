f79f71fb967152598829e02100d43a155334e5eb
Bas Vanmeulebrouk <bas@civity.nl>
2021-09-11 17:51:48 +0200

    Initial commit

---
+++ civity-rest-client/src/main/java/nl/civity/rest/client/ApacheHttpRestClient.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2021, Civity
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nl.civity.rest.client;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.entity.StringEntity;
+
+/**
+ * Implementation of RestClient interface using Apache http components library
+ * @author basvanmeulebrouk
+ */
+public class ApacheHttpRestClient implements RestClient {
+
+    private static final Logger LOGGER = Logger.getLogger(ApacheHttpRestClient.class.getName());
+
+    @Override
+    public String deleteHttp(String uri, Map<String, String> headers, Map<String, String> parameters, int[] expectedReturnCodes) throws RestClientException {
+        String result;
+
+        try {
+            HttpDelete httpDelete = new HttpDelete(buildURI(uri, parameters));
+            setHeaders(httpDelete, headers);
+            result = this.execute(httpDelete, expectedReturnCodes);
+        } catch (URISyntaxException e) {
+            throw new RestClientException("Failed to build a HTTP delete request", e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String getHttp(String uri, Map<String, String> headers, Map<String, String> parameters, int[] expectedReturnCodes) throws RestClientException {
+        String result;
+
+        try {
+            HttpGet httpGet = new HttpGet(buildURI(uri, parameters));
+            setHeaders(httpGet, headers);
+            result = this.execute(httpGet, expectedReturnCodes);
+        } catch (URISyntaxException e) {
+            throw new RestClientException("Failed to build a HTTP get request", e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String patchHttp(String uri, Map<String, String> headers, Map<String, String> parameters, int[] expectedReturnCodes, String body) throws RestClientException {
+        String result;
+
+        try {
+            HttpPatch httpPatch = new HttpPatch(buildURI(uri, parameters));
+            setHeaders(httpPatch, headers);
+            httpPatch.setEntity(this.bodyToHttpEntity(body));
+            result = this.execute(httpPatch, expectedReturnCodes);
+        } catch (URISyntaxException e) {
+            throw new RestClientException("Failed to build a HTTP patch request", e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String postHttp(String uri, Map<String, String> headers, Map<String, String> parameters, int[] expectedReturnCodes, String body) throws RestClientException {
+        String result;
+
+        try {
+            HttpPost httpPost = new HttpPost(buildURI(uri, parameters));
+            setHeaders(httpPost, headers);
+            httpPost.setEntity(this.bodyToHttpEntity(body));
+            result = this.execute(httpPost, expectedReturnCodes);
+        } catch (URISyntaxException e) {
+            throw new RestClientException("Failed to build a HTTP post request", e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String putHttp(String uri, Map<String, String> headers, Map<String, String> parameters, int[] expectedReturnCodes, String body) throws RestClientException {
+        String result;
+
+        try {
+            HttpPut httpPut = new HttpPut(buildURI(uri, parameters));
+            setHeaders(httpPut, headers);
+            httpPut.setEntity(this.bodyToHttpEntity(body));
+            result = this.execute(httpPut, expectedReturnCodes);
+        } catch (URISyntaxException e) {
+            throw new RestClientException("Failed to build a HTTP put request", e);
+        }
+
+        return result;
+    }
+
+    private String checkResponse(CloseableHttpClient httpClient, HttpRequestBase httpRequest, int[] expectedReturnCodes) throws IOException, RestClientException {
+        String result = "";
+
+        try (CloseableHttpResponse response = httpClient.execute(httpRequest)) {
+            int responseCode = response.getStatusLine().getStatusCode();
+
+            if (response.getEntity() != null) {
+                result = (EntityUtils.toString(response.getEntity()));
+            }
+
+            boolean found = false;
+
+            for (Integer expectedReturnCode : expectedReturnCodes) {
+                if (responseCode == expectedReturnCode) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                throw new RestClientException(String.format("Error [%s] executing request to [%s]. The server returned: [%s]", response.getStatusLine(), httpRequest.getURI().toASCIIString(), result));
+            }
+
+            LOGGER.log(Level.INFO, "Successfully executed HTTP request: {0}", httpRequest.getURI().toASCIIString());
+        }
+
+        return result;
+    }
+
+    private void setHeaders(HttpRequestBase httpRequest, Map<String, String> headers) {
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            httpRequest.setHeader(key, value);
+        }
+    }
+
+    private URI buildURI(String uri, Map<String, String> parameters) throws URISyntaxException {
+        URIBuilder result = new URIBuilder(uri);
+
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            result.addParameter(key, value);
+        }
+
+        return result.build();
+    }
+
+    private String execute(HttpRequestBase httpRequest, int[] expectedReturnCodes) throws RestClientException {
+        String result;
+
+        try {
+            try (PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(); CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build()) {
+                result = checkResponse(httpClient, httpRequest, expectedReturnCodes);
+            }
+        } catch (IOException ex) {
+            throw new RestClientException("Error executing request", ex);
+        }
+
+        return result;
+    }
+
+    private HttpEntity bodyToHttpEntity(String body) throws RestClientException {
+        try {
+            return new StringEntity(body);
+        } catch (UnsupportedEncodingException ex) {
+            throw new RestClientException(String.format("Unsupported encoding while creating HttpEntity from String [%s]", body), ex);
+        }
+    }
+}
---
+++ civity-rest-client/src/test/java/nl/civity/rest/client/ApacheHttpRestClientTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2021, Civity
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nl.civity.rest.client;
+
+import java.util.HashMap;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author basvanmeulebrouk
+ */
+public class ApacheHttpRestClientTest {
+
+    private MockWebServer mockWebServer;
+
+    private ApacheHttpRestClient instance;
+    
+    private static final String HELLO_WORLD = "{\"hello\": \"world\"}";
+
+    private static final String ERROR = "{\"error\": \"something went wrong\"}";
+
+    private static final String EMPTY_STRING = "";
+
+    public ApacheHttpRestClientTest() {
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.mockWebServer = new MockWebServer();
+        this.instance = new ApacheHttpRestClient();
+    }
+
+    /**
+     * Test of deleteHttp method, of class ApacheHttpRestClient.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testDeleteHttp() throws Exception {
+        System.out.println("deleteHttp");
+
+        enqueueMockRequest(HELLO_WORLD, 204);
+        String result = instance.deleteHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204});
+        assertEquals(EMPTY_STRING, result);
+
+        Exception exception = assertThrows(RestClientException.class, () -> {
+            enqueueMockRequest(ERROR, 500);
+            instance.deleteHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204});
+        });
+        assertTrue(exception.getMessage().contains(String.format("The server returned: [%s]", ERROR)));
+    }
+
+    /**
+     * Test of getHttp method, of class ApacheHttpRestClient.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testGetHttp() throws Exception {
+        System.out.println("getHttp");
+
+        enqueueMockRequest(HELLO_WORLD, 200);
+        String result = instance.getHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200});
+        assertEquals(HELLO_WORLD, result);
+        
+        Exception exception = assertThrows(RestClientException.class, () -> {
+            enqueueMockRequest(ERROR, 500);
+            instance.getHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200});
+        });
+        assertTrue(exception.getMessage().contains(String.format("The server returned: [%s]", ERROR)));
+    }
+
+    /**
+     * Test of patchHttp method, of class ApacheHttpRestClient.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testPatchHttp() throws Exception {
+        System.out.println("patchHttp");
+
+        enqueueMockRequest(HELLO_WORLD, 200);
+        String result = instance.patchHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204}, HELLO_WORLD);
+        assertEquals(HELLO_WORLD, result);
+
+        enqueueMockRequest(HELLO_WORLD, 204);
+        result = instance.patchHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204}, HELLO_WORLD);
+        assertEquals(EMPTY_STRING, result);
+
+        Exception exception = assertThrows(RestClientException.class, () -> {
+            enqueueMockRequest(ERROR, 500);
+            instance.patchHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204}, HELLO_WORLD);
+        });
+        assertTrue(exception.getMessage().contains(String.format("The server returned: [%s]", ERROR)));
+    }
+
+    /**
+     * Test of postHttp method, of class ApacheHttpRestClient.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testPostHttp() throws Exception {
+        System.out.println("postHttp");
+
+        enqueueMockRequest(HELLO_WORLD, 201);
+        String result = instance.postHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{201}, HELLO_WORLD);
+        assertEquals(HELLO_WORLD, result);
+
+        Exception exception = assertThrows(RestClientException.class, () -> {
+            enqueueMockRequest(ERROR, 500);
+            instance.postHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 202, 204}, HELLO_WORLD);
+        });
+        assertTrue(exception.getMessage().contains(String.format("The server returned: [%s]", ERROR)));
+    }
+
+    /**
+     * Test of putHttp method, of class ApacheHttpRestClient.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testPutHttp() throws Exception {
+        System.out.println("putHttp");
+
+        enqueueMockRequest(HELLO_WORLD, 200);
+        String result = instance.putHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 204}, HELLO_WORLD);
+        assertEquals(HELLO_WORLD, result);  // This should return {"hello": "world"}, but for some reason it does not. 
+
+        enqueueMockRequest(HELLO_WORLD, 204);
+        result = instance.putHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 204}, HELLO_WORLD);
+        assertEquals(EMPTY_STRING, result);  // This should return {"hello": "world"}, but for some reason it does not. 
+
+        Exception exception = assertThrows(RestClientException.class, () -> {
+            enqueueMockRequest(ERROR, 500);
+            instance.putHttp(mockWebServer.url("/").toString(), new HashMap<>(), new HashMap<>(), new int[]{200, 204}, HELLO_WORLD);
+        });
+        assertTrue(exception.getMessage().contains(String.format("The server returned: [%s]", ERROR)));
+    }
+
+    protected void enqueueMockRequest(String data, int responseCode) {
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("Content-Type", "application/json")
+                .setBody(data)
+                .setResponseCode(responseCode));
+    }
+}
