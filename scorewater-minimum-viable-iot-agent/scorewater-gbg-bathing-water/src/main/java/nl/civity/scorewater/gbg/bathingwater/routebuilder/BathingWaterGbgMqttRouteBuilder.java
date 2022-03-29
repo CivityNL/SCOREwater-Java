2ec8d8e7e5b559e7fb255d90476b0466db55cb34
Bas Vanmeulebrouk <bas@civity.nl>
2022-03-29 20:38:04 +0200

    extract methods to superclass

--- scorewater-minimum-viable-iot-agent/scorewater-gbg-bathing-water/src/main/java/nl/civity/scorewater/gbg/bathingwater/routebuilder/BathingWaterGbgMqttRouteBuilder.java
+++ scorewater-minimum-viable-iot-agent/scorewater-gbg-bathing-water/src/main/java/nl/civity/scorewater/gbg/bathingwater/routebuilder/BathingWaterGbgMqttRouteBuilder.java
@@ -8,7 +8,6 @@ package nl.civity.scorewater.gbg.bathingwater.routebuilder;
 import nl.civity.scorewater.fiware.routebuilder.MqttRouteBuilder;
 import nl.civity.scorewater.gbg.bathingwater.processor.BathingWaterGbgFiwareProcessor;
 import org.apache.camel.Processor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,14 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class BathingWaterGbgMqttRouteBuilder extends MqttRouteBuilder {
     
-    @Value("${fiware.publish.contextbroker.url}")
-    private String contextBrokerUrl;
-    
-    @Value("${fiware.publish.contextbroker.service}")
-    private String contextBrokerService;
-
     @Override
     protected Processor createProcessor() {
-        return new BathingWaterGbgFiwareProcessor(contextBrokerUrl, contextBrokerService, null);
+        return new BathingWaterGbgFiwareProcessor(this.getContextBrokerUrl(), this.getContextBrokerService(), null);
     }
 }
