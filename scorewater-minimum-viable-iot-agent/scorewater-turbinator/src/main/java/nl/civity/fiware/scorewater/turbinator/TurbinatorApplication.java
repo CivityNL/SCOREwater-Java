662ceaee24b28a22f08b344eca0daa644d13152e
Bas Vanmeulebrouk <bas@civity.nl>
2022-04-13 09:31:00 +0200

    correct year in header

--- scorewater-minimum-viable-iot-agent/scorewater-turbinator/src/main/java/nl/civity/fiware/scorewater/turbinator/TurbinatorApplication.java
+++ scorewater-minimum-viable-iot-agent/scorewater-turbinator/src/main/java/nl/civity/fiware/scorewater/turbinator/TurbinatorApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Civity BV Zeist
+ * Copyright (c) 2022, Civity BV Zeist
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
