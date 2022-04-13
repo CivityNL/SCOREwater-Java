355a591454335acaae275f5845a7464225cf84c4
Bas Vanmeulebrouk <bas@civity.nl>
2022-04-13 09:48:12 +0200

    use generic password

--- scorewater-minimum-viable-iot-agent/scorewater-turbinator/src/main/resources/sql/00_create_database.sql
+++ scorewater-minimum-viable-iot-agent/scorewater-turbinator/src/main/resources/sql/00_create_database.sql
@@ -2,6 +2,6 @@
  * Create database and database user
  * Usage: psql -U postgres -h host.docker.internal -p 5432 -f 00_create_database.sql
  */
-CREATE USER turbinator WITH PASSWORD 'KanaycsudenBaj';
+CREATE USER turbinator WITH PASSWORD 'password';
 
 CREATE DATABASE turbinator OWNER turbinator;
